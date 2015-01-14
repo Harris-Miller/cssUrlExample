3814b57f374420c67a262798b73bfc08c6e79165
Harris Miller <harrismiller08@gmail.com>
2015-01-13 22:07:13 -0700

    background-image: url() not working with base tag
    with ENV.baseURL in production build, img tag
    src="/assets/images/tomster.png" work
    fine because of the base tag
    
    however css background-image: url('/assets/images/tomster.png') fails
    
    this is when the server is statically serving /dist to a route other than
    the root of the server
    
    css url rewrites are nessessary is ENV.baseURL is set

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+var routes = require('./express/routes/index');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'express/views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use('/cssUrlExample', express.static(path.join(__dirname, 'dist')));
+
+app.use('/', routes);
+
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.info('Express server listening on port ' + server.address().port);
+});
